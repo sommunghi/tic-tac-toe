b2144cd317408a5ee5bb75b63c6e73538c5c126f
sommunghi <sog0740@gmail.com>
2024-02-06 16:09:26 +0900

    completed second pages

---
+++ profile.js
@@ -0,0 +1,46 @@
+const player = [
+    {
+        anchor: document.querySelector("#player-1-anchor"),
+        card: document.querySelector("#input-form1"),
+        inputField: document.querySelector("#player-name1"),
+        nameElement: document.querySelector("#player-1-id"),
+        confirmButton: document.querySelector("#input-form1 .confirm-button"),
+        cancelButton: document.querySelector("#input-form1 button")
+},
+    {
+        anchor: document.querySelector("#player-2-anchor"),
+        card: document.querySelector("#input-form2"),
+        inputField: document.querySelector("#player-name2"),
+        nameElement: document.querySelector("#player-2-id"),
+        confirmButton: document.querySelector("#input-form2 .confirm-button"),
+        cancelButton: document.querySelector("#input-form2 button")
+}
+]
+
+function viewPage (index) {
+    player[index].card.classList.remove("visibility");
+}
+
+function removePage (index) {
+    player[index].card.classList.add("visibility");
+}
+
+function changeName (index) {
+    const inputField = player[index].inputField;
+    if (inputField.value.search(/\W|\s/g) > -1) {
+        inputField.style.backgroundColor = "#f081aa";
+    }
+    else {
+        player[index].nameElement.textContent = inputField.value;
+        inputField.style.backgroundColor = "white";
+        removePage(index);
+    }
+}
+
+for (let i = 0; i < player.length; i++) {
+    player[i].anchor.addEventListener("click", () => viewPage(i));
+    player[i].confirmButton.addEventListener("click", () => changeName(i));
+    player[i].cancelButton.addEventListener("click", () => removePage(i));
+}
+
+
